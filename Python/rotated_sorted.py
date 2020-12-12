ccc1b9a1ce99f977d05d21a3fd62da76ad98e236
Sahil0420 <sahilchandel4200@gmail.com>
2020-12-12 15:47:00 +0530

    Add rotated_sorted on 2020-12-12 15:47:00

---
+++ Python/rotated_sorted.py
@@ -0,0 +1,9 @@
+class Solution:
+	#binary search hai solution
+def main():
+	nums = list(map(int , input().strip().split()))
+	sol = Solution()
+	print(sol.findMin(nums))
+
+if __name__ == '__main__':
+	main()
