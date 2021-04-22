12cfa8b12128eb39889692c87b003c1f7478600f
Sahil0420 <sahilchandel4200@gmail.com>
2021-04-23 04:46:00 +0530

    Add inbuilts on 2021-04-23 04:46:00

---
+++ Python/inbuilts.py
@@ -0,0 +1,22 @@
+# from binarytree import Node             #to build add each node manually
+# from binarytree import build            #to add nodes automatically 
+# from binarytree import tree             #to create a random tree
+
+
+# n=int(input('Enter5 the number of nodes : '))
+# nodes=[]
+#  for x in range(n):
+#      nodes.append(int(input()))
+#  btree = build(nodes)
+#  print(btree)
+# print(btree.inorder)
+
+#USING tree() MODULE TO CREATE A TREE  
+# btree2 = tree()
+# btree2 = tree(height=5,is_perfect=True) 
+# print(btree2)
+# print(btree2.inorder)
+# print(btree2.postorder)
+# print(btree2.preorder)
+
+
