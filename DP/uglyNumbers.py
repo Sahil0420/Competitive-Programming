4cecd9c9f657d3f70304a0b74161bd5e34c8a777
Sahil0420 <sahilchandel4200@gmail.com>
2021-07-29 08:57:00 +0530

    Add uglyNumbers on 2021-07-29 08:57:00

---
+++ DP/uglyNumbers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def noOfNumbers(self, N):
+        # code here
+        pass
+        
+
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range (t):
+        N=int(input())        
+        ob = Solution()
+        print(ob.noOfNumbers(N))
